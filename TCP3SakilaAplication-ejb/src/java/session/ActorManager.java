89fee3b3c4e65fc3df42a7282941cd8249e2cc01
hassanaster <hassanaster2@gmail.com>
2018-09-10 05:37:27 +0200

    Inicio de creación de las vistas

--- TCP3SakilaAplication-ejb/src/java/session/ActorManager.java
+++ TCP3SakilaAplication-ejb/src/java/session/ActorManager.java
@@ -5,8 +5,13 @@
  */
 package session;
 
+import entities.Actor;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -15,7 +20,22 @@ import javax.ejb.LocalBean;
 @Stateless
 @LocalBean
 public class ActorManager {
+ @PersistenceContext(unitName = "TCP3SakilaAplication-ejbPU")
+ private EntityManager em;
+ public void persist(Object object){
+     em.persist(object);
+ }
+ 
+ 
+ public List<Actor> getAllActors() {
+        Query query = em.createNamedQuery("Actor.findAll");
+        return query.getResultList();
+    }
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
+
+    public Actor update(Actor actor) {
+        return em.merge(actor);
+    }
 }
--- TCP3SakilaAplication-war/src/java/managedbeans/ActorMBean.java
+++ TCP3SakilaAplication-war/src/java/managedbeans/ActorMBean.java
@@ -5,9 +5,13 @@
  */
 package managedbeans;
 
+import entities.Actor;
 import javax.inject.Named;
 import javax.enterprise.context.SessionScoped;
 import java.io.Serializable;
+import java.util.List;
+import javax.ejb.EJB;
+import session.ActorManager;
 
 /**
  *
@@ -17,10 +21,45 @@ import java.io.Serializable;
 @SessionScoped
 public class ActorMBean implements Serializable {
 
+    @EJB
+    private ActorManager actorManager;
+
     /**
      * Creates a new instance of ActorMBean
      */
+    
+    private Actor actor; //para mostrar, actualiar e insertar en un formulario
+    private List<Actor> actors; //Para desplegarlos en la base de datos
+    
     public ActorMBean() {
     }
     
+    public List<Actor> getActor(){
+        if ((actors == null)||(actors.isEmpty()))  refresh();
+        return actors;
+    }
+
+    private void refresh() {
+        //tousLesComptes = compteBancaireFacade.findAll();
+    }
+    
+    public Actor getDetails(){
+        return actor;
+    }
+    
+    public String gshowDetails(Actor actor){
+        this.actor = actor;
+        return "ActorDetails";//Se desplegará ActorDetails.xml JSF page
+    }
+    
+    public String update(){
+        System.out.println("###UPDATE###");
+        actor = actorManager.update(actor);
+        return "ActorList";//Se desplegará CustomerDetails.xml JSF page
+    }
+    
+    public String list(){
+        System.out.println("###LIST###");
+        return "ActorList";//Se desplegará CustomerDetails.xml JSF page
+    }
 }
